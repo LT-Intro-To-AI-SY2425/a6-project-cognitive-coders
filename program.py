469af533abd7d9a6b1b895b317d86651cd63a180
NoahDons <nedons@cps.edu>
2025-01-06 11:25:58 -0600

    Format Works

---
+++ program.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import pandas as pd
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+data = pd.read_csv("data.csv")
+print("works")
